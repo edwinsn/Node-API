6fcada5957e13a01d00f638719a48cec69e934be
edwinsn <edwin@sprwt.io>
2023-02-25 20:41:35 -0500

    Checks CRUD controllers

--- config.js
+++ config.js
@@ -5,24 +5,36 @@ const enviroments = {}
 //Default enviroment
 
 enviroments.staging = {
-    httpPort:"3000",
-    httpsPort:"3001",
-    name:"staging",
-    hashingSecret: 'mySecretReallySecret'
+    httpPort: "3000",
+    httpsPort: "3001",
+    name: "staging",
+    hashingSecret: 'mySecretReallySecret',
+    maxChecks: 5,
+    twilio: {
+        accountSid: 'AC4a93f03753a1c507b3f4ebea7feb4998',
+        authToken: 'db4bbf1b98d85cb0e3ecaf21998ea8af',
+        fromPhone: '+1 270 679 1185'
+    }
 }
 
 //Production enviroment
 
 enviroments.production = {
-    httpPort:"8080",
-    httpsPort:"4343",
-    name:"production",
-    hashingSecret: 'mySecretReallySecret'
+    httpPort: "8080",
+    httpsPort: "4343",
+    name: "production",
+    hashingSecret: 'mySecretReallySecret',
+    maxChecks: 5,
+    twilio: {
+        accountSid: 'AC4a93f03753a1c507b3f4ebea7feb4998',
+        authToken: 'db4bbf1b98d85cb0e3ecaf21998ea8af',
+        fromPhone: '+1 270 679 1185'
+    }
 }
 
 //Export enviroment
 
-const currentEnviroment = process.env.NODE_ENV?.toLowerCase() 
+const currentEnviroment = process.env.NODE_ENV?.toLowerCase()
 
 const enviromentToExport = enviroments[currentEnviroment] ? enviroments[currentEnviroment] : enviroments.staging
 
